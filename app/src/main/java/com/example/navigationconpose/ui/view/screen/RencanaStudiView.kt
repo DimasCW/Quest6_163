b600f6df659eece7de12625f8b9b262ac9541c14
DimasCW <cdimas292@gmail.com>
2024-11-22 14:09:44 +0700

    Menambahkan Image dan column berisi 2 text pada Row

--- app/src/main/java/com/example/navigationconpose/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationconpose/ui/view/screen/RencanaStudiView.kt
@@ -1,10 +1,15 @@
 package com.example.navigationconpose.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,8 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigationconpose.R
 import com.example.navigationconpose.model.Mahasiswa
 
@@ -38,7 +48,33 @@ fun RencanaStudyView(
                 .fillMaxWidth()
                 .padding(16.dp), verticalAlignment = Alignment.CenterVertically
         ){
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
 
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            Column (
+                modifier = Modifier
+                    .weight(1f)
+            ){
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
         }
     }
 
