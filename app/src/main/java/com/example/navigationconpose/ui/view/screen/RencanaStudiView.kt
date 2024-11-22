773a58a9a54d84e4d59f0b0512ad89ea5b6c6707
DimasCW <cdimas292@gmail.com>
2024-11-22 13:51:58 +0700

    Menambahkan Column dan membari parameter

--- app/src/main/java/com/example/navigationconpose/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationconpose/ui/view/screen/RencanaStudiView.kt
@@ -1,10 +1,16 @@
 package com.example.navigationconpose.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.navigationconpose.R
 import com.example.navigationconpose.model.Mahasiswa
 
 @Composable
@@ -17,5 +23,13 @@ fun RencanaStudyView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
     var listData : MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+
+    }
 
 }
