7ce7e6dffc1cba9a6e6b712c336628957634f00b
DimasCW <cdimas292@gmail.com>
2024-11-22 13:16:29 +0700

    Membuat Object MataKuliah

--- app/src/main/java/com/example/navigationconpose/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationconpose/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.navigationconpose.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
 }
