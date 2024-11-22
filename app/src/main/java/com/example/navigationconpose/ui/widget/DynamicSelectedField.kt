1abcebd5f132fdf080e840fb35d73dccb023f671
DimasCW <cdimas292@gmail.com>
2024-11-22 13:35:56 +0700

    Selesai Menambahkan ExposedDropdownmenubox dan isinya

--- app/src/main/java/com/example/navigationconpose/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationconpose/ui/widget/DynamicSelectedField.kt
@@ -1,7 +1,15 @@
 package com.example.navigationconpose.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +33,33 @@ fun DynamicSelectTextField(
         onExpandedChange = {expanded = !expanded},
         modifier = Modifier
     ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
 
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {expanded = false}
+        ) {
+            options.forEach {option: String ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        expanded = false
+                        onValueChangedEvent(option)
+                    }
+                )
+            }
+        }
     }
 }
